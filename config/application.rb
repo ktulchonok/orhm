4795c342097db2c946ac8077b3f8b41cf041235c
Max Rostov <max.rostov@do-it.co>
2017-05-16 14:56:51 +0300

    mobile translations

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module History
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :ua
     config.i18n.fallbacks = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
